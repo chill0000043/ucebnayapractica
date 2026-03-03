d09c37a23714cf853d0220acf7b4110760dc9aad
Пягай Рашид Рамисович <stalker0000043@gmail.com>
2026-03-03 10:46:10 +0300

    Отправка отчётов

---
+++ SimpleCalculatorWinForms/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace SimpleCalculatorWinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxNum1 = new System.Windows.Forms.TextBox();
+            this.textBoxNum2 = new System.Windows.Forms.TextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonSubtract = new System.Windows.Forms.Button();
+            this.buttonMultiply = new System.Windows.Forms.Button();
+            this.buttonDivide = new System.Windows.Forms.Button();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxNum1
+            // 
+            this.textBoxNum1.Location = new System.Drawing.Point(11, 12);
+            this.textBoxNum1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxNum1.Name = "textBoxNum1";
+            this.textBoxNum1.Size = new System.Drawing.Size(75, 20);
+            this.textBoxNum1.TabIndex = 0;
+            // 
+            // textBoxNum2
+            // 
+            this.textBoxNum2.Location = new System.Drawing.Point(90, 11);
+            this.textBoxNum2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxNum2.Name = "textBoxNum2";
+            this.textBoxNum2.Size = new System.Drawing.Size(75, 20);
+            this.textBoxNum2.TabIndex = 1;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(11, 35);
+            this.buttonAdd.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 24);
+            this.buttonAdd.TabIndex = 2;
+            this.buttonAdd.Text = "Сложение";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonSubtract
+            // 
+            this.buttonSubtract.Location = new System.Drawing.Point(90, 35);
+            this.buttonSubtract.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonSubtract.Name = "buttonSubtract";
+            this.buttonSubtract.Size = new System.Drawing.Size(75, 24);
+            this.buttonSubtract.TabIndex = 3;
+            this.buttonSubtract.Text = "Вычитание";
+            this.buttonSubtract.UseVisualStyleBackColor = true;
+            this.buttonSubtract.Click += new System.EventHandler(this.buttonSubtract_Click);
+            // 
+            // buttonMultiply
+            // 
+            this.buttonMultiply.Location = new System.Drawing.Point(11, 63);
+            this.buttonMultiply.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonMultiply.Name = "buttonMultiply";
+            this.buttonMultiply.Size = new System.Drawing.Size(75, 24);
+            this.buttonMultiply.TabIndex = 4;
+            this.buttonMultiply.Text = "Умножение";
+            this.buttonMultiply.UseVisualStyleBackColor = true;
+            this.buttonMultiply.Click += new System.EventHandler(this.buttonMultiply_Click);
+            // 
+            // buttonDivide
+            // 
+            this.buttonDivide.Location = new System.Drawing.Point(90, 63);
+            this.buttonDivide.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonDivide.Name = "buttonDivide";
+            this.buttonDivide.Size = new System.Drawing.Size(75, 24);
+            this.buttonDivide.TabIndex = 5;
+            this.buttonDivide.Text = "Деление";
+            this.buttonDivide.UseVisualStyleBackColor = true;
+            this.buttonDivide.Click += new System.EventHandler(this.buttonDivide_Click);
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.labelResult.Location = new System.Drawing.Point(169, 12);
+            this.labelResult.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(22, 17);
+            this.labelResult.TabIndex = 6;
+            this.labelResult.Text = "= ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(316, 184);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.buttonDivide);
+            this.Controls.Add(this.buttonMultiply);
+            this.Controls.Add(this.buttonSubtract);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.textBoxNum2);
+            this.Controls.Add(this.textBoxNum1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Простой калькулятор";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxNum1;
+        private System.Windows.Forms.TextBox textBoxNum2;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonSubtract;
+        private System.Windows.Forms.Button buttonMultiply;
+        private System.Windows.Forms.Button buttonDivide;
+        private System.Windows.Forms.Label labelResult;
+    }
+}
---
+++ SimpleCalculatorWinForms/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SimpleCalculatorWinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+ 
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            PerformOperation((num1, num2) => num1 + num2);
+        }
+
+     
+        private void buttonSubtract_Click(object sender, EventArgs e)
+        {
+            PerformOperation((num1, num2) => num1 - num2);
+        }
+
+
+        private void buttonMultiply_Click(object sender, EventArgs e)
+        {
+            PerformOperation((num1, num2) => num1 * num2);
+        }
+
+  
+        private void buttonDivide_Click(object sender, EventArgs e)
+        {
+            PerformOperation((num1, num2) =>
+            {
+                if (num2 == 0)
+                {
+                    MessageBox.Show("Ошибка: деление на ноль!", "Ошибка",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return 0;
+                }
+                return num1 / num2;
+            });
+        }
+
+        private void PerformOperation(Func<double, double, double> operation)
+        {
+            try
+            {
+           
+                double num1 = Convert.ToDouble(textBoxNum1.Text);
+                double num2 = Convert.ToDouble(textBoxNum2.Text);
+
+                double result = operation(num1, num2);
+
+                labelResult.Text = "= " + result;
+            }
+            catch (FormatException)
+            {
+               
+                MessageBox.Show("Пожалуйста, введите корректные числа!",
+                    "Ошибка ввода", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Произошла ошибка: " + ex.Message,
+                    "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
